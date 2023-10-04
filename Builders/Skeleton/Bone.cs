3cb2340c6e1ee584e668060b455632b7bb38cb0e
tim-dm <imvu_data@zoho.com>
2023-10-04 00:23:39 -0700

    Create Bone.cs
    Created the class that represents a skeleton bone

---
+++ Builders/Skeleton/Bone.cs
@@ -0,0 +1,34 @@
+﻿using System.Xml.Linq;
+
+namespace DMCal3d.Net.Builders.Skeleton
+{
+    public class Bone
+    {
+        private const string BoneElementName = "SUBMESH";
+        private const string NameAttributeName = "NAME";
+        private const string NumChildsAttributeName = "NUMCHILDS";
+        private const string IdAttributeName = "ID";
+
+        public XElement Element { get; private set; }
+
+        public Bone()
+        {
+            Element = new XElement(BoneElementName, string.Empty);
+        }
+
+        public void SetName(string value)
+        {
+            Element.SetAttributeValue(NameAttributeName, value);
+        }
+
+        public void SetNumChilds(int value)
+        {
+            Element.SetAttributeValue(NumChildsAttributeName, value);
+        }
+
+        public void SetId(int value)
+        {
+            Element.SetAttributeValue(IdAttributeName, value);
+        }
+    }
+}
