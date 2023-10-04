e891ceddaf37b90349cef6cca34945bc66c4457d
tim-dm <imvu_data@zoho.com>
2023-10-04 00:18:30 -0700

    Created base SkeletonBuilder class
    Created the groundwork for a skeleton builder

---
+++ Builders/Skeleton/SkeletonBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMCal3d.Net.Builders.Skeleton
+{
+    public class SkeletonBuilder : BaseAssetBuilder
+    {
+        private const string HeaderMagic = "XSF";
+        private const string HeaderVersion = "919";
+        private const string RootName = "SKELETON";
+        private const string DefaultSerializedName = "skeleton";
+
+        public SkeletonBuilder(string serializedName = DefaultSerializedName) : base(serializedName)
+        {
+            AddHeader(HeaderMagic, HeaderVersion);
+            AddRealRoot(RootName);
+        }
+
+        public Bone AddBone()
+        {
+
+        }
+    }
+}
