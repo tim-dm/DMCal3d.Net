980142df2d99844b3cbbb194a79c0479a95accf8
tim-dm <imvu_data@zoho.com>
2023-10-04 05:10:05 -0700

    Moved bone onbects to own folder

--- Builders/Skeleton/Bone/OmniBone.cs
+++ Builders/Skeleton/Bone/OmniBone.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DMCal3d.Net.Builders.Skeleton
+namespace DMCal3d.Net.Builders.Skeleton.Bone
 {
     public class OmniBone : Bone
     {
