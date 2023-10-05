c51d14fa4dbf115ddaca513bc2842ef2b6434db4
tim-dm <imvu_data@zoho.com>
2023-10-04 21:52:39 -0700

    Create Node.cs
    Started laying the groundwork for a node based system

---
+++ Builders/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DMCal3d.Net.Builders
+{
+    public class Node
+    {
+        public XElement Element { get; private set; }
+
+        public Node(string name)
+        {
+            Element = new XElement(name, string.Empty);
+        }
+
+        public Node AddChild(string name)
+        {
+            Node node = new(name);
+            Element.Add(node);
+            return node;
+        }
+    }
+}
