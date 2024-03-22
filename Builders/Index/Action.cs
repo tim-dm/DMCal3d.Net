03ae2d3c323c448d805d52bfda0abfb139b68a37
tim-dm <imvu_data@zoho.com>
2024-03-21 17:07:03 -0700

    Created an action node class for the index builder

---
+++ Builders/Index/Action.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMCal3d.Net.Builders.Index
+{
+    internal class Action : Node
+    {
+        private const string ActionElementName = "Action";
+
+        public Action() : base(ActionElementName) { }
+
+        public Action(string actionId) : base($"Action{actionId}") { }
+        
+        public void SetId(int id)
+        {
+            SetId(id.ToString());
+        }
+
+        public void SetId(string id)
+        {
+            Element.Name = $"Action{id}";
+        }
+    }
+}
