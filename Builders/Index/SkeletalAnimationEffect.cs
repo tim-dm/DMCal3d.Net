16515dd179f40fd07a3a32b6e2e4db5d69f47b19
tim-dm <imvu_data@zoho.com>
2024-03-21 20:51:31 -0700

    Added SkeletalAnimationEffect node

---
+++ Builders/Index/SkeletalAnimationEffect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMCal3d.Net.Builders.Index
+{
+    public class SkeletalAnimationEffect : Node
+    {
+        public SkeletalAnimationEffect(string id) : base($"SkeletalAnimationEffect{id}") { }
+        public SkeletalAnimationEffect() : base($"SkeletalAnimationEffect0") { }
+    }
+}
