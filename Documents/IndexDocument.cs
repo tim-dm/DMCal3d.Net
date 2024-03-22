b96a5cc711630c4e8f0719b980c0ec38dd00e3e5
tim-dm <imvu_data@zoho.com>
2024-03-22 16:11:01 -0700

    Whitespace cleanup

--- Documents/IndexDocument.cs
+++ Documents/IndexDocument.cs
@@ -6,10 +6,7 @@ namespace DMCal3d.Net.Documents
     {
         private const string MeshIndexTagName = "Index";
 
-        public IndexDocument(string cal3dAssetPath, string name = "index.xml") : base(cal3dAssetPath, name)
-        {
-
-        }
+        public IndexDocument(string cal3dAssetPath, string name = "index.xml") : base(cal3dAssetPath, name) { }
 
         public List<XElement>? GetBodyParts()
         {
