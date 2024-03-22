4856ce59273450e502c7f4de9c10d552ffcbb345
tim-dm <imvu_data@zoho.com>
2024-03-21 17:12:58 -0700

    Added a method to create a name element for the action

--- Builders/Index/Action.cs
+++ Builders/Index/Action.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace DMCal3d.Net.Builders.Index
 {
@@ -22,6 +23,13 @@ namespace DMCal3d.Net.Builders.Index
         public void SetId(string id)
         {
             Element.Name = $"Action{id}";
+        }
+
+        public void SetName(string name)
+        {
+            XElement nameElement = Element.GetChild("name", true) ?? new("Name");
+            nameElement.Value = name;
+            Element.Add(nameElement);
         }
     }
 }
