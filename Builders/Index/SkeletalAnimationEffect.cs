d4ef2474c9b64764a5f09bd5ed64a68318cb79be
tim-dm <imvu_data@zoho.com>
2024-03-21 23:59:30 -0700

    Added SetAssetName method to SkeletalAnimationEffect class

--- Builders/Index/SkeletalAnimationEffect.cs
+++ Builders/Index/SkeletalAnimationEffect.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace DMCal3d.Net.Builders.Index
 {
@@ -10,5 +11,14 @@ namespace DMCal3d.Net.Builders.Index
     {
         public SkeletalAnimationEffect(string id) : base($"SkeletalAnimationEffect{id}") { }
         public SkeletalAnimationEffect() : base($"SkeletalAnimationEffect0") { }
+
+        public void SetAssetName(string assetName)
+        {
+            XElement assetNameElement = new("AssetName")
+            {
+                Value = assetName
+            };
+            Element.Add(assetNameElement);
+        }
     }
 }
