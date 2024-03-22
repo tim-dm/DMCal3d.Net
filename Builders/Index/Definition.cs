3c5cca074a5b6c0f93f238dd765decf9662502dc
tim-dm <imvu_data@zoho.com>
2024-03-21 18:54:37 -0700

    Created Definition node

---
+++ Builders/Index/Definition.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DMCal3d.Net.Builders.Index
+{
+    public class Definition : Node
+    {
+        private const string DefinitionElementName = "Definition";
+        private const string ActionTypeElementName = "ActionType";
+
+        public Definition() : base(DefinitionElementName) { }
+
+        public ActionDefinition AddActionDefinition()
+        {
+            ActionDefinition actionDefinition = new();
+            Element.Add(actionDefinition);
+            return actionDefinition;
+        }
+
+        public XElement SetActionType(string type)
+        {
+            XElement ActionType = new(ActionTypeElementName)
+            {
+                Value = type
+            };
+            Element.Add(ActionType);
+            return ActionType;
+        }
+    }
+}
