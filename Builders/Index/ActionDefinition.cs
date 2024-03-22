63aad3b87e8520b891d9099f962b99775b257d58
tim-dm <imvu_data@zoho.com>
2024-03-21 18:49:23 -0700

    Created ActionDefinition node

---
+++ Builders/Index/ActionDefinition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DMCal3d.Net.Builders.Index
+{
+    public class ActionDefinition : Node
+    {
+        private const string ActionDefinitionElementName = "ActionDefinition";
+        private const string ActionAttributesElementName = "ActionAttributes";
+
+        public ActionDefinition() : base(ActionDefinitionElementName) { }
+
+        public XElement AddActionAttributes()
+        {
+            XElement actionAttributes = new(ActionAttributesElementName);
+            Element.Add(actionAttributes);
+            return actionAttributes;
+        }
+    }
+}
