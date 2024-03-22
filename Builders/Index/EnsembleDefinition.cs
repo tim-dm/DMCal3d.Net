34b45d38ef72f716e17967e251c76c18c2ef895c
tim-dm <imvu_data@zoho.com>
2024-03-21 20:51:01 -0700

    Created EnsembleDefinition node

---
+++ Builders/Index/EnsembleDefinition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMCal3d.Net.Builders.Index
+{
+    public class EnsembleDefinition : Node
+    {
+        public EnsembleDefinition(string id) : base($"EnsembleDefinition{id}") { }
+
+        public EnsembleDefinition() : base($"EnsembleDefinition0") { }
+
+        public void SetId(string id)
+        {
+            Element.Name = $"EnsembleDefinition{id}";
+        }
+    }
+}
