1a50eec9ccdedbd16d5bf70a512d672bb19ea549
tim-dm <imvu_data@zoho.com>
2024-03-26 22:15:31 -0700

    Added exception for missing element attributes

---
+++ Exceptions/MissingAttributeException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMCal3d.Net.Exceptions
+{
+    public class MissingAttributeException : Exception
+    {
+        public MissingAttributeException(string element, string attributeName) : base($"{element} is missing a {attributeName} attribute")
+        {
+        }
+    }
+}
