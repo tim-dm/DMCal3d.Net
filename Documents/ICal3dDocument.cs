771b3de544c18163d02e31752f5e15bb129b6dc4
tim-dm <imvu_data@zoho.com>
2024-04-08 11:12:38 -0700

    Added a name property to the interface

--- Documents/ICal3dDocument.cs
+++ Documents/ICal3dDocument.cs
@@ -6,6 +6,11 @@ namespace DMCal3d.Net.Documents
     public interface ICal3dDocument
     {
         /// <summary>
+        /// The name of the document being parsed
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
         /// The xml document
         /// </summary>
         public XDocument Document { get; set; }
